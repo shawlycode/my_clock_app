2d11052153d3ff636b65bebf6aaddb6b75930c1c
Shawlycode <kutinaorlando@gmail.com>
2021-08-30 18:16:40 +0000

    added styles

---
+++ src/Clock.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            date: new Date()
+        };
+    }
+    //define || setup a life-cycle method to class to enable app 
+    // render to webpage for the first time
+    componentDidMount() {
+        this.timerId = setInterval(() => {
+            this.setState({ date: new Date() })
+        }, 1000)
+    }
+    //setup a life-cylcle method to free up timer after closing webpage
+    componentWillUnmount() {
+        clearInterval(this.timerId);
+    }
+    render() {
+        return (
+            <div>
+                <h1>{this.state.date.toLocaleString()}</h1>
+            </div>
+        );
+    }
+}
+
+
+export default Clock;
+
